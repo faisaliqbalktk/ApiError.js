2f52fb6c7bc970d46ee63e38730997b995dfba97
faisal iqbal <faisaliqbal90909@gmail.com>
2024-11-27 04:29:36 +0000

    updated code

--- ApiError.js
+++ ApiError.js
@@ -1,17 +1,21 @@
+//**  Custom error class for API-related errors
 class ApiError extends Error {
     constructor(
-        statusCode, 
-        message = "Something went wrong",
+        statusCode,
+        message = "something went wrong", //* Default error message if not provided
         errors = [],
-        stack = ""
+        stack = "" //* Stack trace for debugging purposes
     ) {
+        //* super method will override the message property of the Error class
         super(message);
+
         this.statusCode = statusCode;
         this.data = null;
         this.message = message;
-        this.success = false;
+        this.success = false; //* Indicates the failure of the corresponding operation
         this.errors = errors;
 
+        //* its good practice to capture the stack trace of the error (Not necessary)
         if (stack) {
             this.stack = stack;
         } else {
